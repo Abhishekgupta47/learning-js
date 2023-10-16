4c7b5e25867b1c57afe9c87ad789df0b4c6216f6
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-10-16 23:04:37 +0530

    learnig_js

---
+++ variable.js
@@ -0,0 +1,20 @@
+// A javaScript variable is simply a name of a storage location. there are two types of variable in java scrippt : local variable and global variable.
+
+
+// there are some rules while declaring a java script variable (also known as identifiers)
+
+// 1. Name must start with a letter (a to z or A to Z),underscore(_),or dollAR($).
+
+// 2. After first letter we can use digits(0 to 9),for example vale 1.
+
+// 3.javaScript variables are case senstive , for example x and X are different variables.
+
+// EXAMPLE
+
+
+
+
+var x = 10;
+var y = 20;
+var z = x+y;
+console.log(z);
